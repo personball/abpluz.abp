efc9ef4c4ee354d197f7dcdad349859fa0b0fffc
王伯承 <personball@163.com>
2020-09-24 11:19:21 +0800

    重新组织LocalizableContent

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -7,5 +7,7 @@ $solutions = "./"
 
 # List of projects
 $projects = (
-    "modules/Abpluz.Abp.Http.Client.SwitchableIdentityClients"
+    "modules/Abpluz.Abp.Http.Client.SwitchableIdentityClients",
+    "modules/Abpluz.Abp.LocalizableContent",
+    "modules/Abpluz.Abp.LocalizableContent.EntityFrameworkCore"
 )
