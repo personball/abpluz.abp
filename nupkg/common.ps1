03bf159593d304ff6316e18b01b1994b5a926fae
王伯承 <personball@163.com>
2021-04-27 17:05:23 +0800

    add project Abpluz.Abp

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -7,6 +7,7 @@ $solutions = "./"
 
 # List of projects
 $projects = (
+    "src/Abpluz.Abp",
     "modules/Abpluz.Abp.Http.Client.SwitchableIdentityClients",
     "modules/Abpluz.Abp.LocalizableContent",
     "modules/Abpluz.Abp.LocalizableContent.EntityFrameworkCore"
